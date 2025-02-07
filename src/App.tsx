f6e43e0abb0a9c140aaba03f8e0df22581dee938
EugeneKGen <eugene_korobkin@mail.ru>
2025-02-07 15:43:10 +0300

    feat: add initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-primary-bg">
+      {/* register page */}
+      <div className={`fixed inset-0 flex justify-center items-center`}>
+        <div className="w-100 bg-secondary-bg rounded-md p-8">
+          <h1 className=" text-2xl font-bold">
+            Регистрация
+          </h1>
+          <p className="">
+            Введите свои учетные данные из системы GREEN-API
+          </p>
+          <div>
+            <label htmlFor="idInstance">idInstance</label>
+            <input placeholder="idInstance" />
+          </div>
+          <div>
+            <label htmlFor="apiTokenInstance">apiTokenInstance</label>
+            <input placeholder="apiTokenInstance" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
